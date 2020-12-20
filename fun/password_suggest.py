82d7f03b2f3d35e43dae6752860c22ade24f0fff
nayan2020 <nayanmahata248@gmail.com>
2020-12-21 00:07:04 +0530

    password creater

---
+++ fun/password_suggest.py
@@ -0,0 +1,32 @@
+from random import randint
+
+def password(l):
+
+    num = '1234567890'
+    salpha = 'abcdefghijklmnopqrstuvwxyz'
+    calpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    cp = '@#$&%(|);'
+    lt = salpha + num + cp +  calpha
+
+    pwd = ''
+    if l < 8:
+        pwd = "YOUR PASSWORD UNDER 8"
+    else:
+        for i in range(l):
+            pwd = lt[randint(0,len(lt))] + pwd
+    return pwd
+
+        
+
+    
+    
+def main():
+    length = int(input("Enter the lenght of password: "))
+    t = password(length)
+    print(t)
+
+
+if __name__ == '__main__':
+    main()    
+
+
