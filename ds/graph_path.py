a9cf729baca42d11876730d924a97c471047e697
nayan2020 <nayanmahata248@gmail.com>
2021-01-21 09:20:48 +0530

    add comments

--- ds/graph_path.py
+++ ds/graph_path.py
@@ -1,46 +1,69 @@
+
 from graph import UndirectedGraph
 from collections import deque
 
 
-def PathTo(graph, a, b):
-    flag = 0
+def PathTo_dfs(graph, src_v, dst_v):
+    """
+    Time complexity O(V+E)
+    space complexity O(V)
+    :param graph:
+    :param src_v:
+    :param dst_v:
+    :return: bool or list
+    """
+    # If the vertex is not present in the graph, return False
+    if not (graph.get_vertex(src_v) and graph.get_vertex(dst_v)):
+        return f'Two vertex is not in same graph'
+    # Identify the visited node, which is visited already
     visited = set()
+
+    # Tracing the path
     dic = dict()
+
+    # add the graph vertex
     stack = deque()
 
-    a_obj = graph.get_vertex(a)
-    stack.append(a_obj)
+    # find the destination and end the loop
+    flag = 0
+
+    src_v_obj = graph.get_vertex(src_v)
+    stack.append(src_v_obj)
     while stack:
-        if flag == 1:
-            break
+
         vertex = stack.pop()
         if vertex in visited:
             continue
 
         visited.add(vertex)
-        for i in vertex.get_neighbour():
-            if i not in visited:
-                stack.append(i)
-                dic[i.get_key()] = vertex.get_key()
-                if i.get_key() == b:
-                    flag = 1
-                    break
+        for neighbour in vertex.get_neighbour():
+            # If the neighbour is not visited then add in stack
+            if neighbour not in visited:
+                stack.append(neighbour)
+                dic[neighbour.get_key()] = vertex.get_key()
+            # If the destination is find then flag become 1
+            if neighbour.get_key() == dst_v:
+                flag = 1
+                # exit from the for loop
+                break
+
         if flag == 1:
+            # exit from the while loop
             break
 
-    if dic.get(b, None) == None:
+    # same graph two node but not connected
+    # if flag == 1 that's mean, destination node is present
+    if flag == 1:
+        k = dst_v
+        lst = list()
+        while k != src_v:
+            lst.append(k)
+            k = dic[k]
+        lst.append(src_v)
+
+        return lst[::-1]
+    else:
         return f'No link Between two node'
-    k = b
-    # print(dic)
-    lst = list()
-    while 1:
-        lst.append(k)
-        if k == a:
-            break
-        k = dic[k]
-
-
-    return lst[::-1]
 
 
 def main():
@@ -50,14 +73,16 @@ def main():
         2: [],
         3: [4, 5],
         4: [6],
-        5: [],
+        5: [40,41,42],
         6: [],
         7: [8],
         8: [],
         9: [10, 11, 12],
         10: [],
         11: [12],
-        12: []
+        12: [],
+        40: [4],
+        41: [4]
     }
 
     undirectedgraph = UndirectedGraph()
@@ -69,10 +94,10 @@ def main():
         for j in k:
             undirectedgraph.add_edge(i, j)
 
-    src = 1
-    des = 4
+    src = 2
+    des = 3
     print(f'{src} to {des}: ', end="")
-    print(PathTo(undirectedgraph, src, des))
+    print(PathTo_dfs(undirectedgraph, src, des))
 
 
 if __name__ == '__main__':
